31f835b70b78aee6eab01854453d2e8fcd994be1
Biljana <Biljana@DESKTOP-BE0CF51>
2019-09-03 00:14:10 +0200

    v3

--- MicroserviceGET/src/main/java/get/Swagger.java
+++ MicroserviceGET/src/main/java/get/Swagger.java
@@ -1,40 +1,45 @@
 package get;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @EnableSwagger2
 @Configuration
 public class Swagger {
 	@Bean
-	public Docket productApi(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("get"))
-				.build()
-				.apiInfo(metaInfo());
-	}
-	private ApiInfo metaInfo() {
-		ApiInfo apiInfo = new ApiInfo("Dental Clinic | (MicroserviceGET) | Backend APRSP Swagger", 
-				"Agilni pristupi u razvoju softverskih proizvoda",
-				"1.0",
-				"",
-				new Contact("Biljana Simovic",
-							"https://github.com/simovicbiljana",
-							"bilja.simovi@gmail.com"),
-				
-				"",
-				"", null);
-		
-		return apiInfo;
-	}
+	public Docket apiDocket() {
 
+		Contact contact = new Contact("Biljana Simovic", 
+				"https://github.com/simovicbiljana",
+				"bilja.simovi@gmail.com");
+
+		List<VendorExtension> vendorExtensions = new ArrayList<>();
+
+		ApiInfo apiInfo = new ApiInfo("Spring Boot Microservices Documentation", 
+				"This pages documents microservice endpoints", 
+				"1.0",
+				"https://github.com/aprsp_dentalClinic/",
+				contact,
+				"", 
+				"", 
+				vendorExtensions);
+
+		Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select()
+				.apis(RequestHandlerSelectors.basePackage("get")).paths(PathSelectors.any()).build();
+
+		return docket;
+
+	}
 }
