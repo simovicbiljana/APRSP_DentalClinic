6174008cb424aedefafdb7b00569bd4f8006c59b
Biljana <Biljana@DESKTOP-BE0CF51>
2019-09-03 23:13:48 +0200

    v3

--- MicroserviceDELETE/src/main/java/delete/MicroserviceDeleteApplication.java
+++ MicroserviceDELETE/src/main/java/delete/MicroserviceDeleteApplication.java
@@ -2,7 +2,12 @@ package delete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceDeleteApplication {
 
--- MicroserviceDELETE/src/main/java/delete/Swagger.java
+++ MicroserviceDELETE/src/main/java/delete/Swagger.java
@@ -1,11 +1,16 @@
 package delete;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,27 +19,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class Swagger {
 	@Bean
-	public Docket productApi(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("delete"))
-				.build()
-				.apiInfo(metaInfo());
-	}
-	private ApiInfo metaInfo() {
-		ApiInfo apiInfo = new ApiInfo("Dental Clinic | (MicroserviceDELETE) | Backend APRSP Swagger", 
-				"Agilni pristupi u razvoju softverskih proizvoda",
-				"1.0",
-				"",
-				new Contact("Biljana Simovic",
-							"https://github.com/simovicbiljana",
-							"bilja.simovi@gmail.com"),
-				
-				"",
-				"");
-		
-		return apiInfo;
-	}
-	
+	public Docket apiDocket() {
 
+		Contact contact = new Contact("Biljana Simovic", 
+				"https://github.com/simovicbiljana",
+				"bilja.simovi@gmail.com");
+
+		List<VendorExtension> vendorExtensions = new ArrayList<>();
+
+		ApiInfo apiInfo = new ApiInfo("Spring Boot Microservices Documentation", 
+				"This pages documents microservice endpoints", 
+				"1.0",
+				"https://github.com/aprsp_dentalClinic/",
+				contact,
+				"", 
+				"", vendorExtensions);
+
+		Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select()
+				.apis(RequestHandlerSelectors.basePackage("delete")).paths(PathSelectors.any()).build();
+
+		return docket;
+
+	}
 }
